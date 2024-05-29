e8951fee6b6c49223c10e766372e3eade172870f
stira <ralf.stich@infoniqa.com>
2024-05-29 17:01:16 +0200

    refactor: Update app component to handle multiple world tickers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ServerSentEventService } from './booking.service';
 import { BehaviorSubject } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { ServerSentEventService } from './ServerSentEventService';
+
+const count = 30;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe, JsonPipe, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -15,22 +17,36 @@ import { AsyncPipe } from '@angular/common';
  * The root component of the Angular application.
  */
 export class AppComponent {
-  constructor(private sseClientService: ServerSentEventService) {
-    const sse = this.sseClientService
-      .getSse()
+  public counts: number[] = [];
+
+  constructor(private sseService: ServerSentEventService) {
+    this.sseService
+      .getAppEvents()
       .pipe()
       .subscribe((event: MessageEvent) => {
-        const value = JSON.parse(event.data).value;
-        this.Message.next(value);
+        const value = JSON.parse(event.data);
+        this.AppEvent.next(value);
       });
+
+    for (let index = 0; index < count; index++) {
+      this.counts.push(Math.floor(Math.random() * 50));
+      this.WorldTicker[index] = new BehaviorSubject<string>('');
+
+      console.log(`world${index}: ${this.counts[index]}`);
+
+      this.sseService
+        .getWorldTicker(`world${index}`, this.counts[index], 100)
+        .pipe()
+        .subscribe((event: MessageEvent) => {
+          const value = JSON.parse(event.data);
+          this.WorldTicker[index].next(`'${value.event}': ${value.payload}`);
+        });
+    }
   }
 
-  /**
-   * The title of the application.
-   */
   title = 'node-rest-test-angular';
-  /**
-   * Represents a behavior subject for the message.
-   */
-  Message = new BehaviorSubject<string>('0');
+
+  WorldTicker: BehaviorSubject<string>[] = [];
+
+  AppEvent = new BehaviorSubject<string>('');
 }
