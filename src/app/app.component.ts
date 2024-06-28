50b654ca58a505248fb78f4b68ecf7c71c125f69
stira <ralf.stich@infoniqa.com>
2024-06-28 16:03:40 +0200

    feat: Add lazy loading for images in app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { ServerSentEventService } from './ServerSentEventService';
 import { ClientSimpleComponent } from './components/client-simple/client-simple.component';
+import { WorldTickerComponent } from './components/world-ticker/world-ticker.component';
 
-const worldCount = 3;
-const clientCount = 15;
+const tickerCount = 6;
+const clientCount = 6;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe, CommonModule, ClientSimpleComponent],
+  imports: [
+    AsyncPipe,
+    JsonPipe,
+    CommonModule,
+    ClientSimpleComponent,
+    WorldTickerComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -18,30 +24,15 @@ const clientCount = 15;
  * The root component of the Angular application.
  */
 export class AppComponent {
-  public counts: number[] = [];
-
-  public clientCounts = Array(clientCount)
+  clientCounts = Array(clientCount)
     .fill(0)
     .map((x, i) => i + 1);
 
-  constructor(private sseService: ServerSentEventService) {
-    for (let index = 0; index < worldCount; index++) {
-      this.counts.push(Math.floor(Math.random() * 500));
-      this.WorldTicker[index] = new BehaviorSubject<string>('');
+  tickerCounts = Array(tickerCount)
+    .fill(0)
+    .map((x, i) => i + 1);
 
-      console.log(`world${index}: ${this.counts[index]}`);
-
-      this.sseService
-        .getWorldTicker(`world${index}`, this.counts[index], 100)
-        .pipe()
-        .subscribe((event: MessageEvent) => {
-          const value = JSON.parse(event.data);
-          this.WorldTicker[index].next(`'${value.event}': ${value.payload}`);
-        });
-    }
-  }
-
-  title = 'node-rest-test-angular';
+  title = 'Node RestTest Angular';
 
   WorldTicker: BehaviorSubject<string>[] = [];
 
