659963198d1babc8c053a01b8087e3103510d082
stira <ralf.stich@infoniqa.com>
2024-07-05 14:58:38 +0200

    refactor: Update styles and dependencies for Angular Material theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ClientSimpleComponent } from './components/client-simple/client-simple.component';
 import { WorldTickerComponent } from './components/world-ticker/world-ticker.component';
-
-const tickerCount = 6;
-const clientCount = 6;
 
 @Component({
   selector: 'app-root',
@@ -24,11 +21,14 @@ const clientCount = 6;
  * The root component of the Angular application.
  */
 export class AppComponent {
-  clientCounts = Array(clientCount)
+  tickerCount = signal(3);
+  clientCount = signal(3);
+
+  clientCounts = Array(this.clientCount())
     .fill(0)
     .map((x, i) => i + 1);
 
-  tickerCounts = Array(tickerCount)
+  tickerCounts = Array(this.tickerCount())
     .fill(0)
     .map((x, i) => i + 1);
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,11 @@ import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync(),
   ],
 };
